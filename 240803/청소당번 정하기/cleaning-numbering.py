a8ca422c44f66ae21ba4a50c154002c5d4590170
jeona05 <g0148220@gmail.com>
2024-08-03 12:13:16 +0900

    Title: 청소당번 정하기, Time: 139ms, Memory: 24MB, Status: Passed - CodeTree

--- 240803/청소당번 정하기/cleaning-numbering.py
+++ 240803/청소당번 정하기/cleaning-numbering.py
@@ -1,11 +1,17 @@
+# 변수 선언 및 입력
 n = int(input())
-a = b = c = 0
-for i in range(0, n, 2):
-    if i % 6 != 0 or i % 12 != 0:
-        a += 1
-for j in range(0, n, 3):
-    if i % 12 != 0:
-        b += 1
-for k in range(0, n, 12):
-    c += 1
-print(a-1, b-1, c-1)
+cnt2, cnt3, cnt12 = 0, 0, 0
+
+# 각 날짜마다 확인합니다.
+for i in range(1, n + 1):
+    # 주기가 가장 긴 12일부터 확인합니다.
+    if i % 12 == 0:
+        cnt12 += 1
+    # 12일 주기에 들어오지 않는다면, 3일 주기에 들어오는지 확인합니다.
+    elif i % 3 == 0:
+        cnt3 += 1
+    # 3일 주기에도 들어오지 않는다면, 2일 주기에 들어오는지 확인합니다.
+    elif i % 2 == 0:
+        cnt2 += 1
+
+print(cnt2, cnt3, cnt12)
