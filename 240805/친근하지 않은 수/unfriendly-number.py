664308852b2d7f77dc463e39b5aea4b2cec5136b
jeona05 <g0148220@gmail.com>
2024-08-05 13:38:42 +0900

    Title: 친근하지 않은 수, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240805/친근하지 않은 수/unfriendly-number.py
@@ -0,0 +1,12 @@
+n = int(input())
+cnt = 0
+for i in range(1, n+1):
+    if i % 2 == 0:
+        continue
+    elif i % 3 == 0:
+        continue
+    elif i % 5 == 0:
+        continue
+    else:
+        cnt += 1
+print(cnt)
