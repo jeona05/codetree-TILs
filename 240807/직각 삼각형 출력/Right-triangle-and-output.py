9b36ee0f586424e4ad2fd704a801a0c91af9b292
jeona05 <g0148220@gmail.com>
2024-08-07 09:48:54 +0900

    Title: 직각 삼각형 출력, Time: 122ms, Memory: 24MB, Status: Passed - CodeTree

--- 240807/직각 삼각형 출력/Right-triangle-and-output.py
+++ 240807/직각 삼각형 출력/Right-triangle-and-output.py
@@ -1,5 +1,5 @@
 n = int(input())
-for i in range(n):
-    for j in range(2*i+1):
+for i in range(n): #n+1로 했을 땐 출력 시 공백이 한 줄 더 나옴...왜
+    for j in range(2*i+1): #2*i-1
         print('*', end="")
     print()
