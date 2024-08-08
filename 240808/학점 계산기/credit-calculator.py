f3457027d89493a3786648a18e467ef76f712a1c
jeona05 <g0148220@gmail.com>
2024-08-08 09:40:28 +0900

    Title: 학점 계산기, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

--- 240808/학점 계산기/credit-calculator.py
+++ 240808/학점 계산기/credit-calculator.py
@@ -1,5 +1,5 @@
 n = int(input())
-arr = list(map(float, input().split()))
+arr = list(map(float, input().split())) # 여기 처음에 int로 썼다가 출력 평균 학점에서 틀림;
 print(f"{sum(arr)/len(arr):.1f}")
 if sum(arr)/len(arr) >= 4.0:
     print("Perfect")
