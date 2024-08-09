f143c76c8d2619ae9e043d676d0860ed5b82a55b
jeona05 <g0148220@gmail.com>
2024-08-09 16:23:38 +0900

    Title: 별표 출력하기 3, Time: 154ms, Memory: 24MB, Status: Passed - CodeTree

--- 240809/별표 출력하기 3/print-star-3.py
+++ 240809/별표 출력하기 3/print-star-3.py
@@ -1,8 +1,8 @@
 n = int(input())
 for i in range(n):
     for j in range(i):
-        print("  ", end="")
+        print("  ", end="") #어이없는 부분: 공백이 두칸이어야되;; 공백한칸은 n=3일때 두번째 줄 공백이 한칸 덜 생기면서 균열 시작
 
-    for j in range(2 * n - 2 * i - 1):
+    for j in range(2 * n - 2 * i - 1): #이것또한이해못함(힌트보고그대로적은거임)
         print("*", end=" ")
     print()
