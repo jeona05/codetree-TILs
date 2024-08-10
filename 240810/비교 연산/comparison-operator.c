98ca6d4c90c88ec07c703eadb52b57f25aea53a6
jeona05 <g0148220@gmail.com>
2024-08-10 11:28:36 +0900

    Title: 비교 연산, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240810/비교 연산/comparison-operator.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    if (a >= b)
+    printf("1\n");
+    else
+    printf("0\n");
+    if (a = b)
+    printf("1\n");
+    else
+    printf("0\n");
+    if (b >= a)
+    printf("1\n");
+    else
+    printf("0\n");
+    if (b > a)
+    printf("1\n");
+    else
+    printf("0\n");
+    if (a == b)
+    printf("1\n");
+    else
+    printf("0\n");
+    if (a != b)
+    printf("1\n");
+    else
+    printf("0\n");
+    return 0;
+}
