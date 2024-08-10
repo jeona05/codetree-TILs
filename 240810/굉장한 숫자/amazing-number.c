8a92489365a0c1ebb08bd3fc4d95809fcaa0baee
jeona05 <g0148220@gmail.com>
2024-08-10 12:02:07 +0900

    Title: 굉장한 숫자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240810/굉장한 숫자/amazing-number.c
+++ 240810/굉장한 숫자/amazing-number.c
@@ -4,7 +4,7 @@ int main() {
     int a;
     scanf("%d", &a);
     if ( (a % 2 == 1 && a % 3 == 0) || (a % 2 == 0 && a % 5 == 0) )
-    printf("ture");
+    printf("true");
     else
     printf("false");
     return 0;
