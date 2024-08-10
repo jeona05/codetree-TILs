4bb010a0836da979856ebed1cae56762401d1b93
jeona05 <g0148220@gmail.com>
2024-08-10 11:00:12 +0900

    Title: 살 수 있는 물건 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240810/살 수 있는 물건 2/things-able-to-buy-2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    scanf("%d", &a);
+    if (a >= 3000)
+    printf("book");
+    else if (a >= 1000)
+    printf("mask");
+    else if (a >= 500)
+    printf("pen");
+    else
+    printf("no");
+    return 0;
+}
