88e85940c6ca3dcc2f03fef85da7f5acc905ad62
jeona05 <g0148220@gmail.com>
2024-08-14 20:31:56 +0900

    Title: 구구단 만들기 2, Time: 139ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240814/구구단 만들기 2/print-multiplication-table-2.py
@@ -0,0 +1,8 @@
+arr = input().split()
+a, b = int(arr[0]), int(arr[1])
+for i in range(2, 9, 2):
+    for j in range(b, a-1, -1):
+        print(f"{j} * {i} = {i*j} ", end="")
+        if j > a:
+            print("/", end=" ")
+    print()
